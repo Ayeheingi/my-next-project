a34d25f8beddbcc882f26234b8a18012cd32b707
Aye Theingi <w25002@osfl.ac.jp>
2026-06-03 15:15:41 +0900

    サイト全体にrevalidateを設定する

--- my-next-project/app/page.tsx
+++ my-next-project/app/page.tsx
@@ -4,55 +4,8 @@ import {getNewsList} from "@/app/_libs/microcms";
 import { TOP_NEWS_LIMIT } from "@/app/components/_constants";
 import NewsList from "@/app/components/NewsList";
 import ButtonLink from "@/app/components/ButtonLink";
-// import { News } from "@/app/_libs/microcms";
-// type News ={
-//   id:string;
-//   title:string;
-//   category:{
-//     name:string;
-//   };
-//   publishedAt:string;
-//   createdAt:string;
-// };
-// const data:{
-//   contents:News[]} = {
-//     contents:[
-//       {
-//         id:"1",
-//         title:
-//         "渋谷にオフィスを移転しました",
-//         category:{
-//           name:"更新情報"
-//         },
-//         publishedAt:"2023/05/19",
-//         createdAt:"2023/05/19",
-//     },
-//     {
-//       id:"2",
-//       title:"当社CEOが業界リーダーTOP30に選出されました",
-//       category:{
-//         name:"更新情報"
-//     },    
-//     publishedAt:"2023/05/19",
-//     createdAt:"2023/05/19",
-//     },
-//     {
-//       id:"3",
-//       title:"テストの記事です",
-//       category:{
-//         name:"更新情報"
-//     },    
-//     publishedAt:"2023/04/19",
-//     createdAt:"2023/04/19",
-//     },
-//     ],
-// };
-// const events = [
-//   { id: "e1", date: "4月 15日", title: "花見" },
-//   { id: "e2", date: "5月 5日", title: "ゴールデンウィークピクニック" },
-//   { id: "e3", date: "8月 20日", title: "バーベキュー" },
-//   { id: "e4", date: "10月 31日", title: "ハロウィン" },
-// ];
+
+export const revalidate = 60;
 
 
 export default async function Home() {
