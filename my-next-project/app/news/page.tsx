8391a122fe56a8d7cb3f81b45e33bd2c86deced8
Aye Theingi <w25002@osfl.ac.jp>
2026-06-03 14:46:18 +0900

    ニュースにSSRをにSSRを適用

--- my-next-project/app/news/page.tsx
+++ my-next-project/app/news/page.tsx
@@ -3,6 +3,8 @@ import NewsList from "../components/NewsList";
 import Pagination from "../components/Pagination";
 import SearchField from "../components/SearchField";
 import { NEWS_LIST_LIMIT } from "../components/_constants";
+
+export const revalidate = 0;
 export default async function News() {
     const {contents:news,totalCount}=await getNewsList({
         limit:NEWS_LIST_LIMIT,
