e527bee94c607f845ed96c67375997fe3b4ee58b
Aye Theingi <w25002@osfl.ac.jp>
2026-06-03 14:58:44 +0900

    ニュースにISRを適用

--- my-next-project/app/news/page.tsx
+++ my-next-project/app/news/page.tsx
@@ -4,7 +4,7 @@ import Pagination from "../components/Pagination";
 import SearchField from "../components/SearchField";
 import { NEWS_LIST_LIMIT } from "../components/_constants";
 
-export const revalidate = 0;
+export const revalidate = 60;
 export default async function News() {
     const {contents:news,totalCount}=await getNewsList({
         limit:NEWS_LIST_LIMIT,
