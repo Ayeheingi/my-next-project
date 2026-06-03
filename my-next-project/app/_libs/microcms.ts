505c3da2ab57e587e780e0c76ae7d753bb92bd00
Aye Theingi <w25002@osfl.ac.jp>
2026-06-03 15:11:33 +0900

    下書きプレビュー時はSSRをする

--- my-next-project/app/_libs/microcms.ts
+++ my-next-project/app/_libs/microcms.ts
@@ -65,6 +65,11 @@ export const getNewsDetail = async (contentId:string,
             endpoint:"news",
             contentId,
             queries,
+            customRequestInit:{
+                next:{
+                    revalidate:queries?.draftKey===undefined?60:0,
+                },
+            },
         });
         return detailData;
     };
